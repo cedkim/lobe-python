0534ca0324a77308d9012e58c3ca6870d3e7e859
Cedric Kim <hello@cedric.today>
2020-12-07 16:06:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         "tflite_runtime ; platform_machine=='armv7l'"
     ],
     dependency_links=[
-        "https://www.piwheels.org/simple/tensorflow",
         "https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl"
     ]
 )
